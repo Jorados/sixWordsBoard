b15960afca58cbf4fa8ca176bfe7bb54ba2f25db
Jorados <100845256+Jorados@users.noreply.github.com>
2022-03-21 12:27:29 +0900

    Add files via upload

---
+++ sixWordsBoard/sixWordsBoard/src/test/java/sixWordsBoard/SixWordsBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package sixWordsBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixWordsBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
