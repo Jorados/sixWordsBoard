9b318c21940c0de714954e256e980428e8097fe1
Jorados <100845256+Jorados@users.noreply.github.com>
2022-03-21 22:38:19 +0900

    Add files via upload

---
+++ SixWordsBoard/SixWordsBoard/src/main/java/SixWordsBoard/SixWordsBoardApplication.java
@@ -0,0 +1,13 @@
+package SixWordsBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixWordsBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixWordsBoardApplication.class, args);
+	}
+
+}
